d0257b58fa8124acb087c28efd955cdff0a9e6ce
Jyotsna <guptajyotsna2002@gmail.com>
2022-05-10 19:34:10 +0530

    Scanner and camera permission done

--- app/src/main/java/com/example/lateentryproject/MainActivity.kt
+++ app/src/main/java/com/example/lateentryproject/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.lateentryproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lateentryproject.UI.FrontPageFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.front_page_fragment)
+        setContentView(R.layout.activity_main)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, FrontPageFragment())
+        transaction.commit()
+
     }
 }
